df77a496069d5f8bf6b90f69e0a8e3a956d9c047
sneseric <79384041+sneseric@users.noreply.github.com>
2023-07-29 13:12:25 -0400

    Update TimeManagementApp.java
    Final

--- TimeManagementApp.java
+++ TimeManagementApp.java
@@ -2,120 +2,92 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class TimeManagementApp extends JFrame {
-    private DefaultListModel<Task> taskListModel;
-    private JList<Task> taskList;
-    private JTextField taskNameField;
-    private JButton addButton;
-    private JLabel progressLabel;
+    private ArrayList<Task> tasks = new ArrayList<>();
 
-    private List<Task> tasks;
+    private JButton addTaskButton;
+    private JButton markAsCompleteButton;
+    private JList<Task> taskList;
+    private DefaultListModel<Task> taskListModel;
+    private JTextField taskNameTextField;
+    private JTextField dueDateTextField;
 
     public TimeManagementApp() {
-        super("Time Management App");
+        setTitle("Time Management App");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponents();
+    }
 
-        tasks = new ArrayList<>();
+    private void initComponents() {
+        // Initialize components
+        addTaskButton = new JButton("Add Task");
+        markAsCompleteButton = new JButton("Mark as Complete");
         taskListModel = new DefaultListModel<>();
         taskList = new JList<>(taskListModel);
-        taskNameField = new JTextField(20);
-        addButton = new JButton("Add Task");
-        progressLabel = new JLabel();
+        taskList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        taskNameTextField = new JTextField(20);
+        dueDateTextField = new JTextField(10);
 
-        // layout
-        setLayout(new BorderLayout());
-        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        topPanel.add(taskNameField);
-        topPanel.add(addButton);
-        add(topPanel, BorderLayout.NORTH);
-        add(new JScrollPane(taskList), BorderLayout.CENTER);
-        add(progressLabel, BorderLayout.SOUTH);
-
-        // Add action listener for add task button
-        addButton.addActionListener(new ActionListener() {
+        // Add action listeners to buttons
+        addTaskButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 addTask();
             }
         });
 
-        // Schedule timer task to update progress label at every second
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        markAsCompleteButton.addActionListener(new ActionListener() {
             @Override
-            public void run() {
-                updateProgress();
+            public void actionPerformed(ActionEvent e) {
+                markAsComplete();
             }
-        }, 0, 1000);
+        });
+
+        // Layout components using panels and layout managers
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+        inputPanel.add(new JLabel("Task Name:"));
+        inputPanel.add(taskNameTextField);
+        inputPanel.add(new JLabel("Due Date (mm-dd-yy):"));
+        inputPanel.add(dueDateTextField);
+        inputPanel.add(addTaskButton);
+
+        JPanel tasksPanel = new JPanel();
+        tasksPanel.setLayout(new BorderLayout());
+        tasksPanel.add(new JScrollPane(taskList), BorderLayout.CENTER);
+        tasksPanel.add(markAsCompleteButton, BorderLayout.SOUTH);
+
+        setLayout(new BorderLayout());
+        add(inputPanel, BorderLayout.NORTH);
+        add(tasksPanel, BorderLayout.CENTER);
     }
 
     private void addTask() {
-        String taskName = taskNameField.getText().trim();
-        if (!taskName.isEmpty()) {
-            Task task = new Task(taskName);
-            tasks.add(task);
-            taskListModel.addElement(task);
-            taskNameField.setText("");
-        }
+        String name = taskNameTextField.getText();
+        String dueDateStr = dueDateTextField.getText();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yy");
+        LocalDate dueDate = LocalDate.parse(dueDateStr, formatter);
+
+        Task task = new Task(name, dueDate);
+        tasks.add(task);
+        taskListModel.addElement(task);
+
+        taskNameTextField.setText("");
+        dueDateTextField.setText("");
     }
 
-    private void updateProgress() {
-        int completedTasks = 0;
-        int totalTasks = tasks.size();
-        for (Task task : tasks) {
-            if (task.isCompleted()) {
-                completedTasks++;
-            }
-        }
-
-        String progressText = "Progress: " + completedTasks + "/" + totalTasks + " tasks completed";
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                progressLabel.setText(progressText);
-            }
-        });
-    }
-
-    private class Task {
-        private String name;
-        private LocalDateTime dueDate;
-        private boolean completed;
-
-        public Task(String name) {
-            this.name = name;
-            this.dueDate = LocalDateTime.now().plusMinutes(5); // Set a default due date to 5 minutes
-            this.completed = false;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public LocalDateTime getDueDate() {
-            return dueDate;
-        }
-
-        public boolean isCompleted() {
-            return completed;
-        }
-
-        public void setCompleted(boolean completed) {
-            this.completed = completed;
-        }
-
-        @Override
-        public String toString() {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formattedDueDate = dueDate.format(formatter);
-            String status = completed ? "[✓]" : "[ ]";
-            return status + " " + name + " (Due: " + formattedDueDate + ")";
+    private void markAsComplete() {
+        int selectedIndex = taskList.getSelectedIndex();
+        if (selectedIndex != -1) {
+            Task selectedTask = taskListModel.getElementAt(selectedIndex);
+            tasks.remove(selectedTask);
+            taskListModel.remove(selectedIndex);
         }
     }
 
@@ -124,10 +96,44 @@ public class TimeManagementApp extends JFrame {
             @Override
             public void run() {
                 TimeManagementApp app = new TimeManagementApp();
-                app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                app.setSize(400, 300);
+                app.pack(); // Automatically resize the window to fit components
                 app.setVisible(true);
             }
         });
+    }
+}
+
+class Task {
+    private String name;
+    private LocalDate dueDate;
+    private boolean isComplete;
+
+    public Task(String name, LocalDate dueDate) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.isComplete = false;
+    }
+
+    // Getters and setters for the name, dueDate, and isComplete
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Due: " + dueDate + ")";
     }
 }
